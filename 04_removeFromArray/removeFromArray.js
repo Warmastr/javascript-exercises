4a76c4b8fc0b28bf5e28aab5f3926fe219a158d6
warmastr <jargyle80@yahoo.com>
2022-05-04 22:45:55 -0500

    completed all calc exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-const removeFromArray = function(array, ...items) {
+/* const removeFromArray = function(array, ...items) {
     for (let i = 1; i < arguments.length; i++) {
         for(let j = array.length - 1; j >= 0; j--) {
             if (arguments[i] === array[j]) {
@@ -7,7 +7,19 @@ const removeFromArray = function(array, ...items) {
         }
     }
     return array;
-};    
+}; */
+
+const removeFromArray = function (...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    // and return that array
+    return newArray;
+};
 console.log(removeFromArray([1, 2, 3], "1", 3));
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(intA, intB) {
+	let result = 0;
+  result = intA + intB;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(intA, intB) {
+	let result = 0;
+  result = intA - intB;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (let i=0; i < arr.length; i++)
+    result = result *= arr[i];
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, pwr) {
+  let result = 1;
+	// num needs to be multiplied by itself pwr times
+  for (let i = 0; i < pwr; i++) {
+    result = result * num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = n;
+    if (n === 0 || n === 1) {
+        result = 1;
+    } else if (n > 1) {
+        while (n > 1) {
+            let prod = n - 1;
+            console.log(result);
+            result = result * prod;
+            n--;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
