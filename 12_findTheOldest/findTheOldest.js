ff0ec3763a46e87f17e537b780b22e153a6ed99a
warmastr <jargyle80@yahoo.com>
2022-05-06 11:44:03 -0500

    findTheOldest completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    /* let currentYear = new Date().getFullYear(); */
+    let sortOldest = array.sort(function(a, b) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].hasOwnProperty('name') === true && array[i].hasOwnProperty('yearOfDeath') === false) {
+                array[i].yearOfDeath = new Date().getFullYear()
+            }
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        if (lastGuy > nextGuy){
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    console.table(sortOldest);
+    return array[0];
 };
 
 // Do not edit below this line
